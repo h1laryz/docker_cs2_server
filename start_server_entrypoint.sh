ab195b4258aadb34fb66fb586300de897901d5c2
h1lary <h1larybusiness7@gmail.com>
2025-05-07 02:34:30 +0000

    test

--- start_server_entrypoint.sh
+++ start_server_entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# some shit without it doesn't work idk (steam = shit)
+if [ -z "$BITS" ]; then
+    # Determine the operating system architecture
+    architecture=$(uname -m)
+
+    # Set OS_BITS based on the architecture
+    if [[ $architecture == *"64"* ]]; then
+        export BITS=64
+    elif [[ $architecture == *"i386"* ]] || [[ $architecture == *"i686"* ]]; then
+        export BITS=32
+    else
+        echo "Unknown architecture: $architecture"
+        exit 1
+    fi
+fi
+
+echo "Shit happening..."
+# https://developer.valvesoftware.com/wiki/Command_line_options
+steamcmd.sh \
+	+api_logging 1 1 \
+	+@sSteamCmdForcePlatformType linux \
+	+@sSteamCmdForcePlatformBitness $BITS \
+	+login anonymous \
+	+quit
+
 echo "/home/container/cs2/game/bin/linuxsteamrt64/cs2 \
     -dedicated \
     -console \
